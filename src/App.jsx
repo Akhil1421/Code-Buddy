0080ad6889ffbd7729750df40aa72e02cc1ef68e
suiisharma <onlycppcode@gmail.com>
2023-09-13 18:03:08 +0530

    Done merging of userprofile

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { BrowserRouter ,Route,Routes } from 'react-router-dom'
 import PassReset from './components/passReset/changePass.jsx';
-import UserProfile from './components/user/UserProfile/UserProfile'
+import UserProfile from './components/user/UserProfile/UserProfile';
 
 function App() {
 
   return (
-    <>
-     <UserProfile></UserProfile>
-    </>
     <BrowserRouter>
       <Routes>
       <Route path="/resetPass" element={<PassReset></PassReset>}/>
+      <Route path="/userProfile" element={<UserProfile></UserProfile>}/>
       </Routes>
     </BrowserRouter>
   )
